a5891ea4b759868d1b225c0256241006c5834982
th14go <thiagoolopezz@gmail.com>
2018-06-13 11:24:01 -0300

    Update sca.sh

--- sca.sh
+++ sca.sh
@@ -5,25 +5,25 @@
 #VARIAVEIS
 DATA=`date +%Y-%m-%d-%H.%M`
 
-SYNC_LOG=/var/log/sca/bcksca.log # local onde o arquivo de log sera armazenado
+SYNC_LOG=/var/log/sca/bckFirebird.log # local onde o arquivo de log sera armazenado
 
 #Gera backup
 echo "******************************" > $SYNC_LOG
-echo "Inicio do Backup do Banco" >> $SYNC_LOG
+echo "Inicio do Backup do Banco Firebird" >> $SYNC_LOG
 date >> $SYNC_LOG
 #echo "******************************" >> $SYNC_LOG
-/opt/firebird/bin/./gbak -user SYSDBA -pass masterkey -v -b /home/administrador/DATABASES/SCA/dados/dados.fdb /home/administrador/DATABASES/SCA/BKP-SCA/bcksca-$DATA.gbak
+/opt/firebird/bin/./gbak -user SYSDBA -pass masterkey -v -b /home/administrador/DATABASES/BASE/dados.fdb /home/administrador/DATABASES/BKP-FDB/bckfdb-$DATA.gbak
 echo "******************************" >> $SYNC_LOG
-echo "Fim do Backup do Banco" >> $SYNC_LOG
+echo "Fim do Backup do Banco Firebird" >> $SYNC_LOG
 date >> $SYNC_LOG
 echo "******************************" >> $SYNC_LOG
 
 #Compacta Backup do banco
 echo "******************************" >> $SYNC_LOG
-echo "Inicio da compactacao do Backup de Log" >> $SYNC_LOG
+echo "Inicio da compactacao do Backup" >> $SYNC_LOG
 date >> $SYNC_LOG
 #echo "******************************" >> $SYNC_LOG
-cd /home/administrador/DATABASES/SCA/BKP-SCA/ 
+cd /home/administrador/DATABASES/BKP-FDB/ 
 tar -cvzf bcksca-$DATA.zip  bcksca-$DATA.gbak
 echo "******************************" >> $SYNC_LOG
 echo "Fim da compactacao da Pasta Dados" >> $SYNC_LOG
@@ -32,17 +32,17 @@ echo "******************************" >> $SYNC_LOG
 
 #Limpa temporarios de backup
 
-rm -rvf /home/administrador/DATABASES/SCA/BKP-SCA/*.gbak
+rm -rvf /home/administrador/DATABASES/BKP-FDB/*.gbak
 
 #Sincroniza com a pasta do servidor com a pasta de backup remoto com o host NTI
-rsync -Cravp /home/administrador/DATABASES/SCA/BKP-SCA/ /mnt/backup/DATABASES/SCA/
-rsync -Cravp /var/log/sca /mnt/backup/LOG/
+rsync -Cravp /home/administrador/DATABASES/BKP-SCA/ /mnt/backup/DATABASES/FIREBIRD/
+rsync -Cravp /var/log/fdb /mnt/backup/LOG/
 
 
 #apaga arquivos com mais de 10 dias Local
 echo "**********INICIO REMOÇÃO LOCAL****************">>$SYNC_LOG
 date >> $SYNC_LOG
-find /home/administrador/DATABASES/SCA/BKP-SCA -mtime +10 -exec rm -rf {} \; >>$SYNC_LOG
+find /home/administrador/DATABASES/BKP-FDB -mtime +10 -exec rm -rf {} \; >>$SYNC_LOG
 date >> $SYNC_LOG
 echo "**********INICIO REMOÇÃO LOCAL*****************">> $SYNC_LOG
 
@@ -50,6 +50,6 @@ echo "**********INICIO REMOÇÃO LOCAL*****************">> $SYNC_LOG
 #ApagandoBckAntigosRemoto
 echo "***********INICIO REMOÇÃO REMOTA***************">>$SYNC_LOG
 date >> $SYNC_LOG
-find /mnt/backup/DATABASES/SCA -mtime +10 -exec rm -rf {} \; >>$SYNC_LOG
+find /mnt/backup/DATABASES/ -mtime +10 -exec rm -rf {} \; >>$SYNC_LOG
 date >> $SYNC_LOG
 echo "**********FIM REMOÇÃO REMOTA*******************">>$SYNC_LOG
